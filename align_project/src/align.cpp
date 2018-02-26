a85856030aa02fdcfa2a537ca3d76a972e606212
alexmihy <alex.mihy@gmail.com>
2018-02-27 00:32:21 +0300

    Base part

--- align_project/src/align.cpp
+++ align_project/src/align.cpp
@@ -10,8 +10,8 @@ using std::tie;
 using std::max;
 using std::min;
 
-#define MSEdRow 15
-#define MSEdCol 15
+#define mdRow 15
+#define mdCol 15
 
 long double calculateMSE(
     Image img1, Image img2, 
@@ -41,7 +41,110 @@ long double calculateMSE(
     return result;
 }
 
-// TODO: functions align two images returns result
+long double calculateCC(
+    Image img1, Image img2, 
+    int row1, int col1,
+    int row2, int col2,
+    int n_rows, int n_cols) 
+{
+    long long sum = 0;
+    for (int i = 0; i < n_rows; i++) {
+        for (int j = 0; j < n_cols; j++) {
+            int r1, g1, b1;
+            int r2, g2, b2;
+            int c1, c2;
+
+            tie(r1, g1, b1) = img1(row1 + i, col1 + j);
+            tie(r2, g2, b2) = img2(row2 + i, col2 + j);
+
+            c1 = (0.2126f * r1 + 0.7152f * g1 + 0.0722f * b1);
+            c2 = (0.2126f * r2 + 0.7152f * g2 + 0.0722f * b2);
+
+            sum += c1 * c2;
+        }
+    }
+
+    long double result = sum;
+
+    return result;
+}
+
+void align_two_image(
+    Image &img1, Image &img2, 
+    int mask1, int mask2, 
+    Image &outImage, bool useMSE) 
+{
+    if (img1.n_rows != img2.n_rows || img1.n_cols != img2.n_cols)
+        return;
+
+    int n_rows  = img1.n_rows;
+    int n_cols = img1.n_cols;
+
+    long double metric = -1.0;
+    int m_row1 = 0, m_col1 = 0, m_row2 = 0, m_col2 = 0, m_n_rows = 0, m_n_cols = 0;
+    for (int dRow = -mdRow; dRow <= mdRow; dRow++) {
+        for (int dCol = -mdCol; dCol <= mdCol; dCol++) {
+            int c_row1 = max(0, dRow); 
+            int c_col1 = max(0, dCol);
+            int c_row2 = min(0, abs(dRow));
+            int c_col2 = min(0, abs(dCol));
+            int c_n_rows = n_rows - abs(dRow);
+            int c_n_cols = n_cols - abs(dCol);
+
+            long double c_metric;
+
+            if (useMSE) {
+                c_metric = calculateMSE(
+                    img1, img2, 
+                    c_row1, c_col1, c_row2, c_col2, c_n_rows, c_n_cols);
+            } else {
+                c_metric = calculateCC(
+                    img1, img2, 
+                    c_row1, c_col1, c_row2, c_col2, c_n_rows, c_n_cols);
+            }
+
+            if (metric < 0 || 
+                (useMSE  && c_metric < metric) ||
+                (!useMSE && c_metric > metric)) {
+                metric = c_metric;
+                m_row1 = c_row1;
+                m_col1 = c_col1;
+                m_row2 = c_row2;
+                m_col2 = c_col2;
+                m_n_rows = c_n_rows;
+                m_n_cols = c_n_cols;
+            }
+        }
+    }
+
+    for (int i = 0; i < m_n_rows; i++) {
+        for (int j = 0; j < m_n_cols; j++) {
+            uint r1, g1, b1, r2, g2, b2;
+
+            tie(r1, g1, b1) = img1(i + m_row1, j + m_col1);
+            tie(r2, g2, b2) = img2(i + m_row2, j + m_col2);
+
+            uint r, g, b;
+            r = g = b = 0;
+
+            if (mask1 & 4)
+                r = r1;
+            if (mask1 & 2)
+                g = g1;
+            if (mask1 & 1)
+                b = b1;
+
+            if (mask2 & 4)
+                r = r2;
+            if (mask2 & 2)
+                g = g2;
+            if (mask2 & 1)
+                b = b2;
+
+            outImage(i, j) = make_tuple(r, g, b);
+        }
+    }
+}
 
 Image align(Image srcImage, bool isPostprocessing, std::string postprocessingType, double fraction, bool isMirror, 
             bool isInterp, bool isSubpixel, double subScale)
@@ -51,101 +154,40 @@ Image align(Image srcImage, bool isPostprocessing, std::string postprocessingTyp
     int out_n_rows = n_rows / 3;
     int out_n_cols = n_cols;
 
+    Image rImage(out_n_rows, out_n_cols);
+    Image gImage(out_n_rows, out_n_cols);
+    Image bImage(out_n_rows, out_n_cols);
+
+    for (int k = 0; k < 3; k++) {
+        for (int i = 0; i < out_n_rows; i++) {
+            for (int j = 0; j < out_n_cols; j++) {
+                uint r, g, b, br;
+
+                tie(r, g, b) = srcImage(k * out_n_rows + i, j);
+                br = (0.2126f * r + 0.7152f * g + 0.0722f * b);
+
+                switch (k) {
+                case 0:
+                    rImage(i, j) = make_tuple(br, br, br);
+                    break;
+                case 1:
+                    gImage(i, j) = make_tuple(br, br, br);
+                    break;
+                case 2:
+                    bImage(i, j) = make_tuple(br, br, br);
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+    }
+
+    Image rgImage(out_n_rows, out_n_cols);
     Image outImage(out_n_rows, out_n_cols);
 
-    long double MSE = -1.0;
-    int MSE_row1 = 0, MSE_col1 = 0, MSE_row2 = 0, MSE_col2 = 0, MSE_n_rows = 0, MSE_n_cols = 0;
-    for (int dRow = -MSEdRow; dRow <= MSEdRow; dRow++) {
-        for (int dCol = -MSEdCol; dCol <= MSEdCol; dCol++) {
-            int c_row1 = max(0, dRow); 
-            int c_col1 = max(0, dCol);
-            int c_row2 = min(0, abs(dRow)) + out_n_rows;
-            int c_col2 = min(0, abs(dCol));
-            int c_n_rows = out_n_rows - abs(dRow);
-            int c_n_cols = out_n_cols - abs(dCol);
-
-
-            long double cMSE = calculateMSE(srcImage, srcImage, c_row1, c_col1, c_row2, c_col2, c_n_rows, c_n_cols);
-
-            if (MSE < 0 || cMSE < MSE) {
-                MSE = cMSE;
-                MSE_row1 = c_row1;
-                MSE_col1 = c_col1;
-                MSE_row2 = c_row2;
-                MSE_col2 = c_col2;
-                MSE_n_rows = c_n_rows;
-                MSE_n_cols = c_n_cols;
-            }
-        }
-    }
-
-    printf("%d, %d, %d, %d, %d, %d\n", MSE_row1, MSE_col1, MSE_row2, MSE_col2, MSE_n_rows, MSE_n_cols);
-
-    for (int i = 0; i < MSE_n_rows; i++) {
-        for (int j = 0; j < MSE_n_cols; j++) {
-            uint r, g, b;
-
-            tie(r, r, r) = srcImage(i + MSE_row1, j + MSE_col1);
-            tie(g, g, g) = srcImage(i + MSE_row2, j + MSE_col2);
-
-            b = 0;
-
-            outImage(i, j) = make_tuple(r, g, b);
-        }
-    }
-
-    MSE = -1.0;
-    for (int dRow = -MSEdRow; dRow <= MSEdRow; dRow++) {
-        for (int dCol = -MSEdCol; dCol <= MSEdCol; dCol++) {
-            int c_row1 = max(0, dRow); 
-            int c_col1 = max(0, dCol);
-            int c_row2 = min(0, abs(dRow)) + 2 * out_n_rows;
-            int c_col2 = min(0, abs(dCol));
-            int c_n_rows = out_n_rows - abs(dRow);
-            int c_n_cols = out_n_cols - abs(dCol);
-
-
-            long double cMSE = calculateMSE(outImage, srcImage, c_row1, c_col1, c_row2, c_col2, c_n_rows, c_n_cols);
-
-            if (MSE < 0 || cMSE < MSE) {
-                MSE = cMSE;
-                MSE_row1 = c_row1;
-                MSE_col1 = c_col1;
-                MSE_row2 = c_row2;
-                MSE_col2 = c_col2;
-                MSE_n_rows = c_n_rows;
-                MSE_n_cols = c_n_cols;
-            }
-        }
-    }
-
-    printf("%d, %d, %d, %d, %d, %d\n", MSE_row1, MSE_col1, MSE_row2, MSE_col2, MSE_n_rows, MSE_n_cols);
-
-    for (int i = 0; i < MSE_n_rows; i++) {
-        for (int j = 0; j < MSE_n_cols; j++) {
-            uint r, g, b;
-
-            tie(r, g, b) = outImage(i + MSE_row1, j + MSE_col1);
-            tie(b, b, b) = srcImage(i + MSE_row2, j + MSE_col2);
-
-            outImage(i, j) = make_tuple(r, g, b);
-        }
-    }
-
-#if 0
-    for (int i = 0; i < out_n_rows; i++) {
-        for (int j = 0; j < out_n_cols; j++) {
-            uint r, g, b;
-
-            tie(r, r, r) = srcImage(i, j);
-            tie(g, g, g) = srcImage(i + out_n_rows, j);
-            tie(b, b, b) = srcImage(i + 2 * out_n_rows, j);
-
-            outImage(i, j) = make_tuple(r, g, b);
-
-        }
-    }
-#endif
+    align_two_image(rImage, gImage, 4, 2, rgImage, true); // 100, 010 => 110
+    align_two_image(rgImage, bImage, 6, 1, outImage, true); // 110, 001 => 111
 
     return outImage;
 }
